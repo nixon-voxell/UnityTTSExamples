4e2cfb9a8bd0116342037a395d81381a20983b28
Nixon <voxell.technologies@gmail.com>
2023-02-27 11:58:37 +0800

    start anew

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    [NonReorderable] public int[] TestArray;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
